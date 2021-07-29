0c8bb12aee2ba70971ca289e4ed2d9a676af3383
Samantha Arellano <Sambollano54@gmail.com>
2021-07-29 03:28:37 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+
 const {ApolloServer} = require('apollo-server-express');
 
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const {authMiddleware} = require("./utils/auth");
+
 const app = express();
-const {typeDefs, resolvers} = require('./Schemas');
-const {authMiddleware} = require('./utils/auth');
 const PORT = process.env.PORT || 3001;
+
+const {typeDefs, resolvers} = require("./schema");
 
 const server = new ApolloServer({ 
   typeDefs, 
@@ -28,5 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      }
+    )
+  }
+);
